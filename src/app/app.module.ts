c950960107e998caa3f4448be3975079e1126211
Rahul Tanwar <rahultanwar706@gmail.com>
2018-01-20 22:51:37 +0530

    account page design complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }          from '@angular/forms';
 import { NgModule } from '@angular/core';
-<<<<<<< Updated upstream
 import { DashboardModule } from './dashboard/dashboard.module';
-=======
 import { RouterModule, Routes } from '@angular/router';
->>>>>>> Stashed changes
 import { AppComponent } from './app.component';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
 import { ForgotPasswordComponent } from './account/forgot-password/forgot-password.component';
-<<<<<<< Updated upstream
 
 
-=======
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { routing } from './app.routing';
+import { SchoolInfoComponent } from './account/school-info/school-info.component';
  
->>>>>>> Stashed changes
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    SchoolInfoComponent
   ],
   imports: [
     BrowserModule,
-<<<<<<< Updated upstream
-    DashboardModule
-=======
+    DashboardModule,
     FormsModule,
     routing
->>>>>>> Stashed changes
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './account/login/login.component';
 import { SignupComponent } from './account/signup/signup.component';
 import { ForgotPasswordComponent } from './account/forgot-password/forgot-password.component';
-
+import { SchoolInfoComponent } from "./account/school-info/school-info.component";
 const appRoutes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'sign-up', component: SignupComponent },
     { path: 'forgot-password', component: ForgotPasswordComponent },
+    { path: 'school-info', component: SchoolInfoComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
