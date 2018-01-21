fcd3c243142e85e4a7bee93317a9ac46488f5a7f
Your Name <you@example.com>
2018-01-21 16:16:00 +0530

    update routing

--- src/app/dashboard/dashboard-routing.ts
+++ src/app/dashboard/dashboard-routing.ts
@@ -13,18 +13,15 @@ import { DashboardComponent } from './dashboard.component';
 
 
  const dashboardRoutes: Routes = [
-     {path: 'dashboard', component:DashboardComponent},
- 
-        {path: 'dashboardmain', component:DashboardMainComponent  },
+        { path: 'dashboard', redirectTo:'/dashboardmain',pathMatch:'full'},
+        { path: 'dashboardmain', component:DashboardMainComponent },
         { path: 'class', component: ClassComponent },
         { path: 'parent', component: ParentComponent },
         { path: 'staff', component: StaffComponent },
         { path: 'callender', component: CalenderComponent },
-        {path: 'student',component:StudentComponent},
+        { path: 'student', component:StudentComponent },
         { path: 'report', component: ReportComponent },
-        {path: 'school',component:SchoolComponent}
-    
-    
+        { path: 'school', component:SchoolComponent }  
 ];
 
 @NgModule({
